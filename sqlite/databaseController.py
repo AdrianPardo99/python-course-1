e4e4c910d81c20b16c4af7cda164db686341154d
Adrian González Pardo <gozapaadr@gmail.com>
2020-08-09 20:18:25 -0500

    SQLite example and controller

--- sqlite/databaseController.py
+++ sqlite/databaseController.py
@@ -8,6 +8,7 @@ import sqlite3
     GitHub: AdrianPardo99
     Licencia Creative Commons CC BY-SA
 
+
     @name       ->  Es el nombre bajo el cual se resguardara el archivo de sqlite3
 """
 def create_database(name):
@@ -44,4 +45,65 @@ def insert_into_table(conexion,table,params,data):
         cursorTable.executemany(f'INSERT INTO {table} VALUES ({params})',data)
     elif type(data) is tuple:
         cursorTable.execute(f'INSERT INTO {table} VALUES ({params})',data)
+    conexion.commit()
+
+"""
+    @conexion   ->  Es la conexion a la base de datos existente gracias al create_database
+    @table      ->  Es el nombre de la tabla que se accedera con el query select*from
+"""
+def select_query_all(conexion,table):
+    cursorTable=conexion.cursor()
+    cursorTable.execute(f'SELECT*FROM {table}')
+    return cursorTable.fetchall()
+
+"""
+    @conexion   ->  Es la conexion a la base de datos existente gracias al create_database
+    @table      ->  Es el nombre de la tabla que se accedera con el query select*from
+    @whereTab   ->  Datos que van en la sentencia where sin hardcoding,
+                    puede igual ponerse operaciones de orden
+    @dataWhere  ->  Tupla de datos que van en el where
+"""
+def select_query_all_with_where(conexion,table,whereTab,dataWhere):
+    cursorTable=conexion.cursor()
+    cursorTable.execute(f'SELECT*FROM {table} WHERE {whereTab}',dataWhere)
+    return cursorTable.fetchall()
+
+"""
+    @conexion   ->  Es la conexion a la base de datos existente gracias al create_database
+    @selectArg  ->  Puede seleccionarse que elementos seran vistos en la consulta
+    @table      ->  Es el nombre de la tabla que se accedera con el query select*from
+"""
+def select_query_2_all(conexion,selectArg,table):
+    cursorTable=conexion.cursor()
+    cursorTable.execute(f'SELECT {selectArg} FROM {table}')
+    return cursorTable.fetchall()
+
+"""
+    @conexion   ->  Es la conexion a la base de datos existente gracias al create_database
+    @selectArg  ->  Puede seleccionarse que elementos seran vistos en la consulta
+    @table      ->  Es el nombre de la tabla que se accedera con el query select*from
+    @whereTab   ->  Datos que van en la sentencia where sin hardcoding,
+                    puede igual ponerse operaciones de orden
+    @dataWhere  ->  Tupla de datos que van en el where
+"""
+def select_query_2_with_where(conexion,selectArg,table,whereTab,dataWhere):
+    cursorTable=conexion.cursor()
+    cursorTable.execute(f'SELECT {selectArg} FROM {table} WHERE {whereTab}',dataWhere)
+    return cursorTable.fetchall()
+
+"""
+    @conexion   ->  Es la conexion a la base de datos existente gracias al create_database
+    @table      ->  Es el nombre de la tabla que se accedera con el query select*from
+    @whereTab   ->  Datos que van en la sentencia where sin hardcoding
+    @dataWhere  ->  Tupla de datos que van en el where
+"""
+def delete_from(conexion,table,whereTab,dataWhere):
+    cursorTable=conexion.cursor()
+    cursorTable.execute(f'DELETE FROM {table} where {whereTab}',dataWhere)
+    conexion.commit()
+
+
+def update_data(conexion,table,dataSet,whereTab,data):
+    cursorTable=conexion.cursor()
+    cursorTable.execute(f'UPDATE {table} SET {dataSet} where {whereTab}',data)
     conexion.commit()
--- sqlite/exec.py
+++ sqlite/exec.py
@@ -5,5 +5,25 @@ con=create_database("example.db")
 create_table_with_params(con,"persona","nombre TEXT,apellidos TEXT,edad INTEGER")
 listaDatos=[("Juan","Martinez",19),("Raymundo","Pulido",21),("Melani","Valdez",22)]
 #print(f'{type(listaDatos) is list}\n{type(listaDatos[0])}')
-insert_into_table(con,"persona","?,?,?",listaDatos)
+#insert_into_table(con,"persona","?,?,?",listaDatos)
+for row in select_query_all(con,"persona"):
+    print(f'{row}')
+
+print("\n\n")
+for row in select_query_all_with_where(con,"persona","edad>=?",(20,)):
+    print(f'{row}')
+
+print("\n\n\n")
+for row in select_query_2_with_where(con,"edad","persona","edad>=?",(20,)):
+    print(f'{row}')
+
+print("\n\n\n")
+delete_from(con,"persona","edad=?",(19,))
+for row in select_query_all(con,"persona"):
+    print(f'{row}')
+
+print("\n\n\n")
+update_data(con,"persona","apellidos=?","nombre=?",("González Pardo","Adrian"))
+for row in select_query_all(con,"persona"):
+    print(f'{row}')
 close_database(con)
