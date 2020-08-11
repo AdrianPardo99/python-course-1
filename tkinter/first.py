dc7aedbcdfaa2eb74fa7145fb857ffba4f67b1a8
Adrian González Pardo <gozapaadr@gmail.com>
2020-08-10 21:35:04 -0500

    First example tkinter

---
+++ tkinter/first.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+root=tkinter.Tk()
+root.title("Firs code")
+# Creacion de frame
+frm=tkinter.Frame(root)
+frm.config(bg="white",width=400,height=400)
+frm.pack()
+
+root.mainloop()
